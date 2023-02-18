4d16c078d0df37198acf0ebb584f2ed96a578d2d
mehdizandian <mahdizandi81@gmail.com>
2023-02-18 16:54:55 +0330

    Navbar✅

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
 import "./main.css";
+
+class App {
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    // Navabar
+    // BMIForm
+    // Result
+  }
+}
+
+const app = new App();
+app.render();
